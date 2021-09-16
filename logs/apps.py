0a6048b21e202eb9e79abc7a7292ad9d3704f574
spoken <abhijitbonik@hotmail.com>
2021-09-16 15:54:56 +0530

    commented xapi signal

--- logs/apps.py
+++ logs/apps.py
@@ -8,4 +8,5 @@ class LogsConfig(AppConfig):
         from .models import TutorialProgress, CourseProgress
         from .signals import tutorial_progress_signal, course_completion_signal
         post_save.connect(tutorial_progress_signal, sender=TutorialProgress, dispatch_uid='trigger_tutorial_progress')
-        post_save.connect(course_completion_signal, sender=CourseProgress, dispatch_uid='trigger_course_completion')
+        #to send course completion signal to Nasscom, just uncommented the following line
+        #post_save.connect(course_completion_signal, sender=CourseProgress, dispatch_uid='trigger_course_completion')
