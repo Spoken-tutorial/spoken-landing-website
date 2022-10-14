d72bd0cb6c1d358d4ef8b1e5c4b4e20dae8a92b8
Shivam <me.shivam998@gmail.com>
2022-10-14 20:14:25 +0530

    forget password

--- spokenlogin/views.py
+++ spokenlogin/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from csc.api.utility import send_pwd_mail
 
 # Create your views here.
+def password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        user = User.objects.get(username=email)
+        send_pwd_mail(user)
+    return render(request, 'registration/password.html')
