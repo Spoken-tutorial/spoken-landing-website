ad790307c78096137f3a621ba41257722ee30d3f
ankitamk14 <ankitamk@gmail.com>
2023-01-30 16:26:20 +0530

    Admin edits (#109)
    * removed media file contents
    
    * order for CSCFossMdlCourses table

--- csc/admin.py
+++ csc/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import FossCategory,CertifiateCategories,CategoryCourses,CSCFossMdlCourses
 # Register your models here.
+
+class CSCFossMdlCoursesAdmin(admin.ModelAdmin):
+    ordering = ['foss__foss']
+    
 admin.site.register(FossCategory)
 admin.site.register(CertifiateCategories)
 admin.site.register(CategoryCourses)
-admin.site.register(CSCFossMdlCourses)
+admin.site.register(CSCFossMdlCourses,CSCFossMdlCoursesAdmin)
 
